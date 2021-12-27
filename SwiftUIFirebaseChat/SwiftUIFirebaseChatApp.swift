f93ca0051f948efe2ee8a0157d6a0c9680af65ad
Uğur Kırmacı <kirmaciugur@gmail.com>
2021-12-27 18:06:27 +0300

    Initial Commit

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by UğurKırmacı on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
